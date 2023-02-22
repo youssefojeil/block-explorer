e8396602cb8a9ee8a787a291af1df607c0854f8c
unknown <youssef.ojeil@hotmail.com>
2023-02-21 19:51:55 -0500

    get latest block number, get current gas price and format to gwei, get latest transactions from current block

--- src/App.js
+++ src/App.js
@@ -1,36 +1,54 @@
-import { Alchemy, Network } from 'alchemy-sdk';
-import { useEffect, useState } from 'react';
+import { Alchemy, Network, Utils } from "alchemy-sdk";
+import { useEffect, useState } from "react";
 
-import './App.css';
+import "./App.css";
 
-// Refer to the README doc for more information about using API
-// keys in client-side code. You should never do this in production
-// level code.
 const settings = {
   apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
   network: Network.ETH_MAINNET,
 };
 
-
-// In this week's lessons we used ethers.js. Here we are using the
-// Alchemy SDK is an umbrella library with several different packages.
-//
-// You can read more about the packages here:
-//   https://docs.alchemy.com/reference/alchemy-sdk-api-surface-overview#api-surface
 const alchemy = new Alchemy(settings);
 
 function App() {
   const [blockNumber, setBlockNumber] = useState();
+  const [gasPrice, setGasPrice] = useState();
+  const [latestTransactions, setLatestTransactions] = useState([]);
 
   useEffect(() => {
-    async function getBlockNumber() {
-      setBlockNumber(await alchemy.core.getBlockNumber());
+    async function getCurrentBlockNumber() {
+      const blockNumber = await alchemy.core.getBlockNumber();
+      setBlockNumber(blockNumber);
+      // console.log(blockNumber);
+
+      const block = await alchemy.core.getBlockWithTransactions(blockNumber);
+
+      console.log(block.transactions);
+      setLatestTransactions(block.transactions);
     }
 
-    getBlockNumber();
-  });
+    async function getCurrentGasPrice() {
+      const gasPrice = await alchemy.core.getGasPrice();
+      const gasPriceGwei = Utils.formatUnits(gasPrice._hex, "gwei");
+      setGasPrice(gasPriceGwei);
+      // console.log(gasPrice._hex);
+      // console.log(Utils.formatUnits(gasPrice._hex, "gwei"));
+    }
 
-  return <div className="App">Block Number: {blockNumber}</div>;
+    getCurrentBlockNumber();
+    getCurrentGasPrice();
+  }, []);
+
+  return (
+    <div>
+      <div className="App">Block Number: {blockNumber}</div>
+      <div className="App"> Gas Price: {gasPrice}</div>
+
+      {latestTransactions.map((transaction) => (
+        <li>{transaction.hash}</li>
+      ))}
+    </div>
+  );
 }
 
 export default App;
